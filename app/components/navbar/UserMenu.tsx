1592e945f0e3d131f1f8ddd04727f03e7efcb96b
Petya Angelova <bedfordpetya@gmail.com>
2023-06-07 23:47:59 +0300

    Modal component for user registration

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,15 +2,17 @@
 
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
+import { useRegisterModal } from "@/app/hooks/useRegisterModal";
 
 const UserMenu = () => {
+  const registerModal = useRegisterModal();
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleOpen = () => {
+  const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
-  };
+  }, []);
 
   return (
     <div
@@ -65,7 +67,7 @@ const UserMenu = () => {
           <div className="flex flex-col cursor-pointer">
             <>
               <MenuItem onClick={() => {}} label="Login" />
-              <MenuItem onClick={() => {}} label="Sign Up" />
+              <MenuItem onClick={registerModal.onOpen} label="Sign Up" />
             </>
           </div>
         </div>
