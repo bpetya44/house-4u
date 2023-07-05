c62eea13f971fc0a53d07040838c94f6ac6fb8f1
Petya Angelova <bedfordpetya@gmail.com>
2023-07-05 20:10:26 +0300

    Enhance: Error page added

---
+++ app/error.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useEffect } from "react";
+import EmptyState from "./components/EmptyState";
+
+interface ErrorStateProps {
+  error: Error;
+}
+
+const ErrorState: React.FC<ErrorStateProps> = ({ error }) => {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <EmptyState
+      title="Oh no! Something went wrong."
+      subtitle="Please try again later"
+    />
+  );
+};
