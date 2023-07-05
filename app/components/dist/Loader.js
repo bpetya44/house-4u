310f02e3fef8ad221e5e1b36628eb63c5cfa8405
Petya Angelova <bedfordpetya@gmail.com>
2023-07-05 19:57:35 +0300

    Enhance: Loader added

---
+++ app/components/dist/Loader.js
@@ -0,0 +1,9 @@
+"use client";
+"use strict";
+exports.__esModule = true;
+var react_spinners_1 = require("react-spinners");
+var Loader = function () {
+    return (React.createElement("div", { className: "\r\n        h-[70vh]\r\n        flex\r\n        flex-col\r\n        justify-center\r\n        items-center        \r\n        " },
+        React.createElement(react_spinners_1.DotLoader, { color: "#a855f7", size: 100 })));
+};
+exports["default"] = Loader;
---
+++ app/dist/loading.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var Loader_1 = require("./components/Loader");
+var Loading = function () {
+    return React.createElement(Loader_1["default"], null);
+};
+exports["default"] = Loading;
