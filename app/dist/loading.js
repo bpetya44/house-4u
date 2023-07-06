4639e5839b7afefbb31922858f6024e2cbc25c9a
Petya Angelova <bedfordpetya@gmail.com>
2023-07-06 18:28:38 +0300

    removed dist

--- app/dist/loading.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Loader_1 = require("./components/Loader");
-var Loading = function () {
-    return React.createElement(Loader_1["default"], null);
-};
-exports["default"] = Loading;
